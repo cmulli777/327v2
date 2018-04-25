c4206315bf3872ea4a1038b3a5157fdc53882628
Cole <cmulli@iastate.edu>
2018-04-24 19:17:22 -0500

    Fixs

--- 03_ExerciseHelp/calculator.js
+++ 03_ExerciseHelp/calculator.js
@@ -3,7 +3,7 @@
 //
 //
 
-// 
+//
 var Calc = {
 
 Model : {
@@ -13,7 +13,31 @@ Model : {
 View : {
   textRow : {id: "textRow", type: "text", value: "", onclick:""},
   button7 : {id: "button7", type: "button", value: 7, onclick:""},
-  button8 : {id: "button8", type: "button", value: 8, onclick:""}
+  button8 : {id: "button8", type: "button", value: 8, onclick:""},
+  button9 : {id: "button9", type: "button", value: 9, onclick:""},
+  buttonplus : {id: "buttonplus", type: "button", value: '+', onclick:""},
+  button4 : {id: "button4", type: "button", value: 4, onclick:""},
+  button5 : {id: "button5", type: "button", value: 5, onclick:""},
+  button6 : {id: "button6", type: "button", value: 6, onclick:""},
+  buttonminus : {id: "buttonminus", type: "button", value: '-', onclick:""},
+  button4 : {id: "button4", type: "button", value: 4, onclick:""},
+  button5 : {id: "button5", type: "button", value: 5, onclick:""},
+  button6 : {id: "button6", type: "button", value: 6, onclick:""},
+  buttonminus : {id: "buttonminus", type: "button", value: '-', onclick:""},
+  button1 : {id: "button1", type: "button", value: 1, onclick:""},
+  button2 : {id: "button2", type: "button", value: 2, onclick:""},
+  button3 : {id: "button3", type: "button", value: 3, onclick:""},
+  buttonmult : {id: "buttonmult", type: "button", value: '*', onclick:""},
+  button0 : {id: "button0", type: "button", value: 0, onclick:""},
+  buttondec : {id: "buttondec", type: "button", value: '.', onclick:""},
+  buttoneq : {id: "buttoneq", type: "button", value: '=', onclick:""},
+  buttondiv : {id: "buttondiv", type: "button", value: '/', onclick:""},
+  buttonC : {id: "buttonC", type: "button", value: 'C', onclick:""},
+  buttonMR : {id: "buttonMR", type: "button", value: 'MR', onclick:""},
+  buttonMM : {id: "buttonMM", type: "button", value: 'M-', onclick:""},
+  buttonMP : {id: "buttonMP", type: "button", value: 'M+', onclick:""},
+  buttonMC : {id: "buttonMC", type: "button", value: 'MC', onclick:""}
+
 },
 
 Controller : {
@@ -44,13 +68,56 @@ display : function() {
   s += "<tr><td>" + Calc.displayElement(Calc.View.textRow) + "</td></tr>";
   s += "<tr><td>";
   s += Calc.displayElement(Calc.View.button7);
+  s += "&nbsp&nbsp";
   s += Calc.displayElement(Calc.View.button8);
-  s += "</tr></td></table>";
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.button9);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.buttonplus);
+  s += "<tr><td>";
+  s += Calc.displayElement(Calc.View.button3);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.button5);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.button6);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.buttonminus);
+  s += "</td></tr>"
+  s += "<tr><td>";
+  s += Calc.displayElement(Calc.View.button1);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.button2);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.button3);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.buttonmult);
+  s += "</td></tr>"
+  s += "<tr><td>";
+  s += Calc.displayElement(Calc.View.button0);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.buttondec);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.buttoneq);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.buttondiv);
+  s += "</td></tr>"
+  s += "<tr><td>";
+  s += Calc.displayElement(Calc.View.buttonC);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.buttonMR);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.buttonMM);
+  s += "&nbsp&nbsp";
+  s += Calc.displayElement(Calc.View.buttonMP);
+  s += "</td></tr>"
+  s += "<tr><td>";
+  s += Calc.displayElement(Calc.View.buttonMC);
+  s += "</td></tr></table>";
   return s;
 },
 
 attachHandlers : function() {
-  Calc.View.button7.onclick = "Calc.button7Handler()"; 
+  Calc.View.button7.onclick = "Calc.button7Handler()";
 },
 
 button7Handler : function() {
